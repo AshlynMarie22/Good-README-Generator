73ddb1f229bfff11d254f1161521ec34cd5ef1f9
Ashlyn Smith <ashlyn.marie.smith2@gmail.com>
2020-09-24 19:38:13 -0400

    Added starting files. Index.js file has questions added.

---
+++ Develop/index.js
@@ -0,0 +1,55 @@
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your ReadMe title?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "What is your ReadMe description?"
+      },
+      {
+        type: "input",
+        name: "table",
+        message: "What is your ReadMe table of contents?"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What is your ReadMe usage?"
+    },
+    {
+         type: "input",
+         name: "license",
+        message: "Which liscense would you like to use with your ReadMe?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who contributed to your ReadMe?"
+    },
+    {
+         type: "input",
+         name: "tests",
+        message: "How to test your ReadMe?"
+    },
+    {
+        type: "input",
+        name: "questions",
+       message: "Questions for your ReadMe?"
+   },
+];    
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
