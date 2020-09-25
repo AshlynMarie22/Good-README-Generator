5b752bec701673d0748fecfafadbf3ce949e1b3f
Ashlyn Smith <ashlyn.marie.smith2@gmail.com>
2020-09-25 01:44:08 -0400

    updated generatMarkdown template

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,50 +1,50 @@
 // function to generate markdown for README
+const bashStart = "```bash";
+const bashEnd = "````";
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-# <img src="https://media.giphy.com/media/3og0INs7kEnoBYDGNi/giphy.gif" width="30px"> ${data.title} <img src="https://media.giphy.com/media/3og0INs7kEnoBYDGNi/giphy.gif" width="30px">
+  return `# <img src="https://media.giphy.com/media/3og0INs7kEnoBYDGNi/giphy.gif" width="30px"> ${data.title} <img src="https://media.giphy.com/media/3og0INs7kEnoBYDGNi/giphy.gif" width="30px">
 ![anything](https://img.shields.io/badge/license-${data.license}-${data.color})
+
 ## Table of Contents
 
 * [Description](#description)
 * [Installation](#installation)
 * [Preview Website](#preview-website)
 * [How to Use](#how-to-use)
-* [Known Issues](#known-issues)
+* [Contributing](#contributing)
 * [License](#mit-license)
 
 
 ## Description
 
-This site allows you to search for a specific city to find the current weather and the next five days. It also shows the uv index which is color coded depending on the uv rays strength. The site also saves your search history for quick access. 
+${data.description}
 
 ## Installation
 
 Clone this repo by using:
 
-```bash
-git clone https://github.com/AshlynMarie22/Weather_Dashboard.git
-```
+${bashStart}
+git clone ${data.installation}
+${bashEnd}
 
 Next check to make sure there weren't any problems:
-
-```bash
+${bashStart}
 git status
-```
+${bashEnd}
 
 If there are no problems you are ready to add it:
 
-```bash
+${bashStart}
 git add .
-```
+${bashEnd}
 
 Now that the repo is cloned you can preview the website. Open the cloned repo folder in Visual Studio.
 
 If you have the shortcut installed:
 
-```bash
+${bashStart}
 code .
-```
+${bashEnd}
 
 Open the index.html file.
 
@@ -65,18 +65,16 @@ https://ashlynmarie22.github.io/Weather_Dashboard/
 
 
 ## How to Use:
-1. Type in the city you would like to search for and click enter:
+${data.usage}
 
-![firststep](https://ashlynimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-09-16+at+12.43.18+AM.png)
+## Contributing:
+${data.contributing}
 
-2. Continue to search cities for their weather forecast:
-![secondstep](https://ashlynimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-09-16+at+1.13.16+AM.png)
+## Testing:
+${data.tests}
 
-3. To revist a city previously viewed, click the city and then click the search button again. 
-
-## Known Issues:
-When you click on the city button to revist the weather forecast you must also click the search button again. 
-
+## Questions?:
+If you have questions regarding ${data.title}, contact me at: ${data.questions}
 
 ---
 
